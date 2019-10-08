79b669f102d83ab65d4fe1ac0b8c4b3fea3919e0
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-10-08 14:29:41 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,12 @@
-<?PHP
-
-session_start();
-
-session_destroy();
-
-//Limpa
-unset($_SESSION['login']);
-unset($_SESSION['senha']);
-unset($_SESSION['tipo']);
-
-//Redireciona para a página de autenticação
-header('location:index.php');
+<?php
+session_start();
+session_destroy();
+
+//Limpa
+unset($_SESSION['login']);
+unset($_SESSION['senha']);
+unset($_SESSION['tipo']);
+
+//Redireciona para a página inicial
+header('location:index.php');
 ?>
