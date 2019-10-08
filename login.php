e9e2dbc8561659b64048780ce6987eca1e09bbd3
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-10-08 14:29:22 -0300

    Update login.php

--- login.php
+++ login.php
@@ -5,6 +5,7 @@ include "conexao.php";
 
 // session_start inicia a sessão
 session_start();
+
 // as variáveis login e senha recebem os dados digitados na página anterior
 $login = $_POST['login'];
 $senha = $_POST['senha'];
