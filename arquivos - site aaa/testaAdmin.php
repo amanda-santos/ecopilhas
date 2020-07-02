ef0f1a31fc72dc4cacc3d985d58e3dde04de90a3
Amanda Santos <amandasf0200@gmail.com>
2020-07-02 15:45:42 -0300

    update README.md

--- arquivos - site aaa/testaAdmin.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	function isAdmin($id){
-	  if (($id == 3) || ($id == 4) || ($id == 6) || ($id == 7) || ($id == 8) || ($id == 9)){      
-		  return true;
-	  }else{
-		return false;
-	  }
-	} 
-?>
